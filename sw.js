e68303deadda85a525dc1004e7d9c1f4b97dfa79
Muhammedko90 <kocm942@gmail.com>
2025-06-15 13:07:27 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE_NAME = 'mak-taksit-cache-v4'; // Önbellek sürümünü güncelledik
+const urlsToCache = [
+  '/',
+  '/index.html',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap',
+  'https://cdn.jsdelivr.net/npm/dayjs@1/dayjs.min.js',
+  'https://cdn.jsdelivr.net/npm/dayjs@1/plugin/customParseFormat.js',
+  'https://cdn.jsdelivr.net/npm/chart.js',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => {
+        console.log('Önbellek açıldı');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Eski önbellekleri temizle
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(name => name !== CACHE_NAME).map(name => caches.delete(name))
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      }
+    )
+  );
+});
